7d31435db334e1794a0541224a00a03d57d0c27d
David Gaither <gaither@protonmail.com>
2024-02-02 14:27:47 -0500

    Linear search complete

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,17 @@
-def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+bananas_list = list('bananas')
 
-def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+def linear_search(char, arr):
+    for i in range(len(arr)):
+        if arr[i] == char:
+            return i
+    return None
+    
+
+def linear_search_global(char, arr):
+    result = []
+    for i in range(len(arr)):
+        if arr[i] == char:
+            result.append(i)
+    return result
+
+print(linear_search(3, [1,2,3]))
